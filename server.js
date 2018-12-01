8e18ac1063c4ec2be8df9b91ff2f36b2d23f5144
Nestor Guemez Che <nestoragc@hotmail.com>
2018-12-01 16:50:19 -0600

    Angular root path fixed

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/testApp'));
 
 app.listen(process.env.PORT || 8080);
 
 // Path location strategy to give Angular control over the routing inside our app
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/testApp/index.html'));
 });
 
 console.log("Console listening");
